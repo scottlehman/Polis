66466f6810cedb9e558825e97eb8177de1b9a151
Scott Lehman <scottsgllehman@gmail.com>
2019-05-14 20:27:22 -0600

    first commit and push to establish Heroku deploy

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from "react";
+import LandingPage from './LandingPage';
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <LandingPage />
+      </Fragment>
+    );
+  }
+}
+
+export default App;
