ada5896a1222230cdcc0a0b4a60fddda30a6185c
rstutalo <richardtutalo@gmail.com>
2019-05-19 21:06:08 -0600

    redid folder structure for components for easier layout and styling. Also added Grid component to better leverage Bootstrap responsiveness for frontend

---
+++ client/src/Grid/Grid.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+// export react components for bootstrap vals of Container, Col, and Rows
+
+
+//export bootstrap container for easy access in other components
+export function Container({ fluid, children }) {
+    return <div className={`container${fluid ? "-fluid" : ""}`}>{children}</div>;
+}
+
+//export Row component for easy access in other components
+export function Row ({ fluid, children }) {
+    return <div className={`row${fluid ? "-fluid" : ""}`}>{children}</div>;
+}
+
+//export Col component for easy access in other components
+export function Col ({ size, children }) {
+    return (
+        <div
+            className={size
+            .split(" ")
+            .map(size => "col-" + size)
+            .join(" ")}
+        >
+            {children}
+        </div>
+    );
+}
