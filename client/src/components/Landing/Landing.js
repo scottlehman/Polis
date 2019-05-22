811f80a00089e31beb50d711efda37a0b693bd25
rstutalo <richardtutalo@gmail.com>
2019-05-21 20:27:40 -0600

    landing page layout skeleton complete and linked with corresponding stylesheet

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import "./Landing.css";
 
 function Landing() {
   return (
-    <div className="App">
-      <h1>Landing</h1>
-    </div>
+    <div className="App container text-center">
+      <h1>Polis</h1>
+      <h2>Company Logo Goes Here</h2>
+      <div className="btn-selection">
+        <button className="candidate">Get Candidate Info</button>
+        <button className="election">Get Election Info</button>
+      </div> 
+    </div> // END OF APP SECTION
   );
 }
 
