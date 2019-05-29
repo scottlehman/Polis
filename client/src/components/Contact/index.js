13a16fc5e543cfa8dcfc065ce440d690eb6038cf
Scott Lehman <scottsgllehman@gmail.com>
2019-05-28 21:23:01 -0600

    updated the contact page with a cleaner react layout

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -1,41 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Contact() {
-  return (
-    <div className="contact">
-      <h1>Contact</h1>
-      <div class="contact-form">
-        {/* Will need to link database if we want to store user's names and passwords */}
-        <form action="" method="post">
 
-          {/* setup for first name */}
-          <div>
-            <label for="first-name">First Name</label>
-            <input type="text" class="first-name" name="firstname" placeholder="Enter Your First Name"></input>
-          </div>
 
-          {/* setup for last name */}
-          <div>
-            <label for="last-name">Last Name</label>
-            <input type="text" class="last-name" name="lastname"></input>
+class Contact extends Component {
+
+  handleSubmit = event => {
+    console.log("does it work?")
+  }
+
+  render() {
+      return (
+        <div className="contact">
+          <h1>Contact</h1>
+          <div className="contact-form">
+            {/* Will need to link database if we want to store user's names and passwords */}
+            <form action="" method="post">
+    
+              {/* setup htmlFor first name */}
+              <div>
+                <label htmlFor="first-name">First Name</label>
+                <input type="text" className="first-name" name="firstname" placeholder="Enter Your First Name"></input>
+              </div>
+    
+              {/* setup htmlFor last name */}
+              <div>
+                <label htmlFor="last-name">Last Name</label>
+                <input type="text" className="last-name" name="lastname" placeholder="Enter Your Last Name"></input>
+              </div>
+            
+              {/* setup htmlFor email address */}
+              <div>
+                <label htmlFor="email">Email Address</label>
+                <input type="email" className="email-signup" name="emailaddress" placeholder="Enter Email Address"></input>
+              </div>
+            
+              {/* setup for user messages  */}
+              <div>
+                <label htmlFor="message">Message:</label>
+                <textarea className="message" name="user-message"></textarea>
+              </div>
+              <button onClick={this.handleSubmit}>Create Account</button>
+            </form>
           </div>
-        
-          {/* setup for email address */}
-          <div>
-            <label for="email">Email Address</label>
-            <input type="email" class="email-signup" name="emailaddress" placeholder="Enter Email Address"></input>
-          </div>
-        
-          {/* setup for user messages  */}
-          <div>
-            <label for="message">Message:</label>
-            <textarea class="message" name="user-message"></textarea>
-          </div>
-          
-        </form>
-      </div>
-    </div> // END OF CONTACT DIV
-  );
-}
+        </div> // END OF CONTACT DIV
+      );
+    }
+
+  }
 
 export default Contact;
